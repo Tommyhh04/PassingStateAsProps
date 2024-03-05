597f08e2ad3a710e60be584be3d8932a12d14b75
Tommyhh04 <tommyhh04.kodehode@gmail.com>
2024-03-05 09:36:25 +0100

    Adding comments

--- stateProp/src/App.jsx
+++ stateProp/src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-
+import Count from "./Count";
 //This app is a simple counter. I have followed the videos from scrimba to aid me in making this. This is practice of passing state as props
 
 function App() {
   //Gives function to the counter
-
+  //Stores the state of count
   const [count, setCount] = React.useState(0);
-
+  //Makes the + button usable by increasing the number by 1
   function add() {
     setCount((prevCount) => prevCount + 1);
   }
--- stateProp/src/Count.jsx
+++ stateProp/src/Count.jsx
@@ -1,9 +1,10 @@
-import React from "react"
+import React from "react";
 
-export default function Count(props){
-    return(
-        <div className="counter">
-            <h1>{props.number}</h1>
-        </div>
-    )
+//Counter display exported to App.jsx. This is not really necesarry as it is so small but it is to practice import export and using props
+export default function Count(props) {
+  return (
+    <div className="counter">
+      <h1>{props.number}</h1>
+    </div>
+  );
 }
